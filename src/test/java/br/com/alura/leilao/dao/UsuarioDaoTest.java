6f1409741e9b0666d0eafb0fa02553318754aca2
LucasDev13 <lucaspontes140@gmail.com>
2021-08-23 10:15:58 -0300

    Escrevendo mais um cenário de teste para o usuaio.

--- src/test/java/br/com/alura/leilao/dao/UsuarioDaoTest.java
+++ src/test/java/br/com/alura/leilao/dao/UsuarioDaoTest.java
@@ -6,6 +6,7 @@ import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 
 import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -14,7 +15,7 @@ class UsuarioDaoTest {
     private UsuarioDao usuarioDao;
 
     @Test
-    void testeBuscaDeUsuarioPeloUsername(){
+    void deveriaEncontrarUsuarioCadastrado(){
         EntityManager manager = JPAUtil.getEntityManager();
         this.usuarioDao = new UsuarioDao(manager);
 
@@ -25,6 +26,20 @@ class UsuarioDaoTest {
 
         Usuario encontrado = this.usuarioDao.buscarPorUsername("fulano");
         Assert.assertNotNull(encontrado);
+    }
+
+    @Test
+    void naoDeveriaEncontrarUsuarioNaoCadastrado(){
+        EntityManager manager = JPAUtil.getEntityManager();
+        this.usuarioDao = new UsuarioDao(manager);
+
+        Usuario usuario = new Usuario("fulano", "fulano@email.com", "12345678");
+        manager.getTransaction().begin();
+        manager.persist(usuario);
+        manager.getTransaction().commit();
+
+        Assert.assertThrows(NoResultException.class, () -> this.usuarioDao.buscarPorUsername("beltrano"));
+
     }
 
 }
