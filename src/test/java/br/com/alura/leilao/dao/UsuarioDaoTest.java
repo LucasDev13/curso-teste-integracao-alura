a7da6fd809d0bc454e16e431b7eaf9324089024d
LucasDev13 <lucaspontes140@gmail.com>
2021-08-23 10:05:14 -0300

    Configurado o persistence.xml, o EntityManager.

--- src/test/java/br/com/alura/leilao/dao/UsuarioDaoTest.java
+++ src/test/java/br/com/alura/leilao/dao/UsuarioDaoTest.java
@@ -1,6 +1,7 @@
 package br.com.alura.leilao.dao;
 
 import br.com.alura.leilao.model.Usuario;
+import br.com.alura.leilao.util.JPAUtil;
 import org.junit.Assert;
 import org.junit.jupiter.api.Test;
 
@@ -11,13 +12,19 @@ import static org.junit.jupiter.api.Assertions.*;
 class UsuarioDaoTest {
 
     private UsuarioDao usuarioDao;
-    private EntityManager manager;
 
     @Test
     void testeBuscaDeUsuarioPeloUsername(){
+        EntityManager manager = JPAUtil.getEntityManager();
         this.usuarioDao = new UsuarioDao(manager);
-        Usuario usuario = this.usuarioDao.buscarPorUsername("fulano");
-        Assert.assertNotNull(usuario);
+
+         Usuario usuario = new Usuario("fulano", "fulano@email.com", "12345678");
+         manager.getTransaction().begin();
+         manager.persist(usuario);
+         manager.getTransaction().commit();
+
+        Usuario encontrado = this.usuarioDao.buscarPorUsername("fulano");
+        Assert.assertNotNull(encontrado);
     }
 
 }
